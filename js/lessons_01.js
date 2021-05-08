f77d134684bff5c5d0d645a63d0abd528c0f1d5f
Denys Filichkin <denys.philichkin@gmail.com>
2021-05-08 18:58:42 +0300

    first-js

---
+++ js/lessons_01.js
@@ -0,0 +1,21 @@
+let a = 5;
+console.log(a);
+a = Number(a);
+console.log(typeof a);
+
+
+
+const name = "Denys";
+console.log(name);
+
+
+let x = 5;
+x = 10;
+
+console.log(x);
+// Логическое И
+console.log(5 && 6 && "world");
+//  Логическое ИЛИ
+console.log(false || 6 || null || 65);
+//  Логическое НЕ
+console.log(!false);
