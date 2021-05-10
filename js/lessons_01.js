ce9ab49880bf1b34be9e35a8121241bff1db820a
Denys Filichkin <denys.philichkin@gmail.com>
2021-05-10 21:38:12 +0300

    prttr

--- js/lessons_01.js
+++ js/lessons_01.js
@@ -1,21 +1,75 @@
-let a = 5;
-console.log(a);
-a = Number(a);
-console.log(typeof a);
+// let a = 5;
+// console.log(a);
+// a = Number(a);
+// console.log(typeof a);
 
 
 
-const name = "Denys";
-console.log(name);
+// const name = "Denys";
+// console.log(name);
 
 
-let x = 5;
-x = 10;
+// let x = 5;
+// x = 10;
 
-console.log(x);
-// Логическое И
-console.log(5 && 6 && "world");
-//  Логическое ИЛИ
-console.log(false || 6 || null || 65);
-//  Логическое НЕ
-console.log(!false);
+// console.log(x);
+// // Логическое И
+// console.log(5 && 6 && "world");
+// //  Логическое ИЛИ
+// console.log(false || 6 || null || 65);
+// //  Логическое НЕ
+// console.log(!false);
+
+/*
+ * Просим клиента подтвердить бронь на отель
+ * и сохраняем в переменную результат работы confirm
+ */
+// const isComing = confirm('Please confirm hotel reservation');
+// console.log(isComing);
+
+
+
+// Один из встроенных объектов в язык. 
+// Предоставляет набор методов для работы с числами.
+// Знание всех методов наизусть не требуется, только некоторых, наиболее полезных.
+
+
+//  Math.floor(num) - возвращает наибольшее целое число, меньшее, либо равное указанному
+console.log(Math.floor(1.7)); // 1
+
+// Math.ceil(num) - возвращает наименьшее целое число,
+// большее, либо равное указанному числу.
+console.log(Math.ceil(1.2)); // 2
+
+// Math.round(num) - возвращает значение числа, округлённое до ближайшего целого
+console.log(Math.round(1.2)); // 1
+console.log(Math.round(1.5)); // 2
+
+// Math.max(num1, num2, ...) - возвращает наибольшее число из набора
+console.log(Math.max(20, 10, 50, 40)); // 50
+
+// Math.min(num1, num2, ...) - возвращает наименьшее число из набора
+console.log(Math.min(20, 10, 50, 40)); // 10
+
+// Math.pow(base, exponent) - возведение в степень
+console.log(Math.pow(2, 4)); // 16
+
+// Math.random() - возвращает псевдослучайное число в диапазоне [0, 1)
+console.log(Math.random()); // случайное число между 0 и 1
+console.log(Math.random() * (10 - 1) + 1); // случайное число от 1 до 10
+
+/*
+ * Используем цикл для вывода только нечетных чисел.
+ * Для чётных i срабатывает continue, выполнение тела прекращается
+ * и управление передаётся на следующую итерацию.
+ */
+const number = 10;
+
+for (let i = 0; i < number; i += 1) {
+  if (i % 2 === 0) {
+    continue;
+  }
+
+  console.log('Нечетное i: ', i); // 1, 3, 5, 7, 9
+}
+
