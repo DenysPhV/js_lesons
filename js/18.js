0787fb9fc26da6090336cc4c08c711f6d125410e
Denys Filichkin <denys.philichkin@gmail.com>
2021-05-11 17:59:10 +0300

    19

---
+++ js/18.js
@@ -0,0 +1,13 @@
+function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+  let message;
+  // Change code below this line
+  const totalPrice = pricePerDroid * orderedQuantity;
+  
+  if (customerCredits < totalPrice) {
+    message = "Insufficient funds!"
+  } else {
+    message = `You ordered ${orderedQuantity} droids, you have ${customerCredits - totalPrice} credits left`
+  }
+  // Change code above this line
+  return message;
+}
---
+++ js/19.js
@@ -0,0 +1,14 @@
+function checkPassword(password) {
+  const ADMIN_PASSWORD = 'jqueryismyjam';
+  let message;
+
+  if (password === null) { // Change this line
+    message =  'Canceled by user!';
+  } else if (password === ADMIN_PASSWORD) { // Change this line
+    message = 'Welcome!';
+  } else {
+    message = 'Access denied, wrong password!';
+  }
+
+  return message;
+}
