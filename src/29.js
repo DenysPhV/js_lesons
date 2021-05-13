9723fc88a8f54ed069359f969cf0a0abcb75ebbd
Denys Filichkin <denys.philichkin@gmail.com>
2021-05-13 16:31:53 +0300

    29

---
+++ src/29.js
@@ -0,0 +1,33 @@
+function getShippingCost(country) {
+  let message;
+  let price;
+  // Change code below this line
+
+  switch (country) {
+    case 'China':
+      price = 100;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
+
+    case 'Chile':
+      price = 250;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
+
+    case 'Australia':
+      price = 170;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
+
+    case 'Jamaica':
+      price = 120;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
+
+    default:
+      message = 'Sorry, there is no delivery to your country';
+
+      // Change code above this line
+      return message;
+  }
+}
