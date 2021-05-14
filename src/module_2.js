ea91934d1361ff34e52b1c44f6679525e42fd87e
Denys Filichkin <denys.philichkin@gmail.com>
2021-05-14 20:27:25 +0300

    Update module_2.js

---
+++ src/module_2.js
@@ -0,0 +1,12 @@
+/* 
+Задача: Крайние элементы массива
+Задание
+Напиши функцию getExtremeElements(array) которая принимает один параметр array - массив элементов произвольной длины. Функция должна возвращать массив из двух элементов - первого и последнего элемента параметра array.
+
+Тесты
+Объявлена функция getExtremeElements(array)
+Вызов getExtremeElements([1, 2, 3, 4, 5]) возвращает [1, 5]
+Вызов getExtremeElements(["Earth", "Mars", "Venus"]) возвращает ["Earth", "Venus"]
+Вызов getExtremeElements(["apple", "peach", "pear", "banana"]) возвращает ["apple", "banana"]
+*/
+// answer
