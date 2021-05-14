1896b960dae5c373f75f31fb81bdd4e5f77dc83e
Denys Filichkin <denys.philichkin@gmail.com>
2021-05-14 19:27:14 +0300

    Update module_2.js

---
+++ src/module_2.js
@@ -0,0 +1,18 @@
+/*
+Длина массива
+Длина массива, то есть число его элементов, хранится в свойстве length. Это динамическая величина, которая изменяется автоматически при добавлении или удалении элементов.
+
+const planets = ["Earth", "Mars", "Venus"];
+console.log(planets.length); // 3
+Задание
+Объяви переменную fruitsArrayLength и присвой ей длину массива fruits используя свойство length.
+
+Тесты
+Объявлена переменная fruitsArrayLength
+Значение переменной fruitsArrayLength это число 4
+*/
+// answer
+const fruits = ['apple', 'peach', 'pear', 'banana'];
+
+// Change code below this line
+const fruitsArrayLength = fruits.length;
