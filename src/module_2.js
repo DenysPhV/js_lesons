9c78bdcbc17436ecb78752b2a4d0f56594d0fa2c
Denys Filichkin <denys.philichkin@gmail.com>
2021-05-15 15:53:13 +0300

    split

---
+++ src/module_2.js
@@ -0,0 +1,28 @@
+/*
+Метод строк split()
+Метод split(delimeter) позволяет превратить строку в массив, «разбив» его по разделителю delimeter. Если разделитель это пустая строка, то получится массив отдельных символов. Разделителем может быть один или несколько символов.*/
+
+// const name = 'Mango';
+// console.log(name.split('')); // ["M", "a", "n", "g", "o"]
+
+// const message = 'JavaScript essentials';
+// console.log(message.split(' ')); // ["JavaScript", "essentials"]
+
+/*Задание
+Дополни код функции splitMessage(message, delimeter) так, чтобы она возвращала в переменной words результат разделения строки message по разделителю delimeter - массив строк.
+
+Тесты
+Объявлена функция splitMessage(message, delimeter)
+Вызов splitMessage("Mango hurries to the train", " ") возвращает ["Mango", "hurries", "to", "the", "train"]
+Вызов splitMessage("Mango", "") возвращает ["M", "a", "n", "g", "o"]
+Вызов splitMessage("best_for_week", "_") возвращает ["best", "for", "week"]
+*/
+// answer
+function splitMessage(message, delimeter) {
+  let words;
+  // Change code below this line
+  words = message.split(delimeter);
+
+  // Change code above this line
+  return words;
+}
