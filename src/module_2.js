0fab358a9ad115ab43c48a029f9087fa5f942072
Denys Filichkin <denys.philichkin@gmail.com>
2021-05-16 10:55:23 +0300

    Update module_2.js

---
+++ src/module_2.js
@@ -0,0 +1,38 @@
+/*
+Метод slice()
+Метод slice(begin, end) возвращает новый массив, содержащий копию части исходного массива, не изменяя его. Копия делается от begin и до, но не включая, end - индексы элементов исходного массива.
+
+Если begin и end не указаны, будет создана полная копия исходного массива.
+Если не указан end, копирование будет от start и до конца исходного массива.
+Если значение start отрицательное, а end не указан, то будут скопированы последние N элементов.
+const planets = ["Earth", "Mars", "Venus", "Jupiter", "Saturn"];
+
+console.log(planets.slice(0, 2)); // ['Earth', 'Mars']
+console.log(planets.slice(0, 4)); // ['Earth', 'Mars', 'Venus', 'Jupiter']
+console.log(planets.slice(1, 3)); // ['Mars', 'Venus']
+console.log(planets.slice(-2)); // ['Jupiter', 'Saturn']
+console.log(planets.slice()); // ['Earth', 'Mars', 'Venus', 'Jupiter', 'Saturn']
+Задание
+Дополни код так, чтобы переменные содержали частичные копии исходного массива fruits.
+
+firstTwoEls - массив из первых двух элементов
+nonExtremeEls - массив из всех элементов кроме первого и последнего
+lastThreeEls - массив из трёх последних элементов
+Тесты
+Объявлена переменная fruits
+Значение переменной fruits это массив ["apple", "plum", "pear", "orange", "banana"]
+Объявлена переменная firstTwoEls
+Значение переменной firstTwoEls это массив ["apple", "plum"]
+Объявлена переменная nonExtremeEls
+Значение переменной nonExtremeEls это массив ["plum", "pear", "orange"]
+Объявлена переменная lastThreeEls
+Значение переменной lastThreeEls это массив ["pear", "orange", "banana"]
+Переменной lastThreeEls присвоена копия части массива fruits после применения метода slice с правильными аргументами
+*/
+// answer
+const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+
+// Change code below this line
+const firstTwoEls = fruits.slice(0, 2);
+const nonExtremeEls = fruits.slice(1, 4);
+const lastThreeEls = fruits.slice(-3);
