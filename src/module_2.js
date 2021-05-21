2ed3a427cfe5c1f594373304655e7a6f87705b9a
Denys Filichkin <denys.philichkin@gmail.com>
2021-05-21 12:20:45 +0300

    25

--- src/module_2.js
+++ src/module_2.js
@@ -21,6 +21,11 @@
 function getCommonElements(array1, array2) {
   // Change code below this line
   const newArray = [];
+  for (let i = 0; i <= array1.length; i += 1) {
+    if (array2.includes(array1[i])) {
+      newArray.push(array1[i]);
+    }
+  }
 
   return newArray;
   // Change code above this line
--- src/myself.js
+++ src/myself.js
@@ -1,15 +1,25 @@
-//const message ="JavaScript is in my blood";
+/*
+Напиши функцию getCommonElements(array1, array2) которая получает два массива произвольной длины в параметры array1 и array2, и возвращает новый массив, состоящий из тех элементов, которые присутствуют в обоих исходных массивах.
 
-//const pricePerWord = 10;
-//const result = message.split(" ").length;
+*/
+const array1 = [1, 2, 3];
+const array2 = [10, 2, 30, 40];
 
+function getCommonElements(array1, array2) {
+  // Change code below this line
+  const newArray = [];
+  for (let i = 0; i <= array1.length; i += 1) {
+    if (array2.includes(array1[i])) {
+      newArray.push(array1[i]);
+    }
+  }
 
-//console.log(result * pricePerWord);
-
-const numbers = [];
-
-for (let i = 0; i < 3; i += 1) {
-  numbers.push(`label-${i}`);
+  return newArray;
+  // Change code above this line
 }
 
-console.log('numbers: ', numbers); // ['label-0', 'label-1', 'label-2']
+// console.log(array1);
+// console.log(array2);
+// console.log(array1.includes(2));
+// console.log(array2.includes(2));
+// getCommonElements([1, 2, 3], [10, 20, 30]);
