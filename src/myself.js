482680a9482f18197ba0150062ef2ec4d76f788a
Denys <77440293+DenysPhV@users.noreply.github.com>
2021-05-22 23:32:24 +0300

    update myself.js

--- src/myself.js
+++ src/myself.js
@@ -1,20 +1,14 @@
-// объявление объекта { ключ:значения }
 const hotel = {
   name: 'Resort Hotel',
   stars: 5,
   capacity: 100,
 };
 
-//доступ к свойствам
-//          object.key
-console.log(hotel.name); // Resort Hotel
-//          object.key
-console.log(hotel['name']); // Resort Hotel
+hotel.greet = function () {
+  console.log('Welcome!');
+};
 
-hotel.name = 'Coastline Resort';
-console.log(hotel.name); // Coastline Resort
-console.log(hotel['name']); // Coastline Resort
+hotel.greet(); // Welcome!
+Hotel
 
-hotel['name'] = 'Stardust Hotel';
-console.log(hotel.name); // Stardust Hotel
-console.log(hotel['name']); // Stardust Hotel
+
