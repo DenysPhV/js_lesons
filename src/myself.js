62a1063b5473fd5549d78a130e01f7f222a3e62b
Denys Filichkin <denys.philichkin@gmail.com>
2021-05-28 19:14:14 +0300

    Update myself.js

--- src/myself.js
+++ src/myself.js
@@ -1,6 +1,15 @@
+<<<<<<< Updated upstream
 const houses = ['Arryn', 'Frey', 'Greyjoy', 'Stark', 'Lannister', 'Tyrell'];
 const copyOfHouses = [...houses];
 
 console.log(houses); // ['Arryn','Frey','Greyjoy','Stark','Lannister','Tyrell']
 console.log(copyOfHouses); // ['Arryn','Frey','Greyjoy','Stark','Lannister','Tyrell']
 console.log(houses === copyOfHouses); // false - разные ссылки
+=======
+const rgb = [200, 255, 100];
+
+const [red, ...colors] = rgb;
+
+console.log(`Red: ${red}, Colors: ${colors}`);
+// Red: 200, Colors: [255, 100]
+>>>>>>> Stashed changes
