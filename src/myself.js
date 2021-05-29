563830289d4f9a7dbc16e2429cb07519e08d81e2
Denys Filichkin <denys.philichkin@gmail.com>
2021-05-29 19:33:08 +0300

    Update myself.js

--- src/myself.js
+++ src/myself.js
@@ -1,15 +1,5 @@
-<<<<<<< Updated upstream
-const houses = ['Arryn', 'Frey', 'Greyjoy', 'Stark', 'Lannister', 'Tyrell'];
-const copyOfHouses = [...houses];
+const a = [1, 2];
+const b = [0, 30];
 
-console.log(houses); // ['Arryn','Frey','Greyjoy','Stark','Lannister','Tyrell']
-console.log(copyOfHouses); // ['Arryn','Frey','Greyjoy','Stark','Lannister','Tyrell']
-console.log(houses === copyOfHouses); // false - разные ссылки
-=======
-const rgb = [200, 255, 100];
-
-const [red, ...colors] = rgb;
-
-console.log(`Red: ${red}, Colors: ${colors}`);
-// Red: 200, Colors: [255, 100]
->>>>>>> Stashed changes
+const c = [20, ...a, ...b, 30, 50];
+console.log(c);
