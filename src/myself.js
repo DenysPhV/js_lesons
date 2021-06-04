5da9572f107ef1b2c79971654267584f65a7c15a
Denys Filichkin <denys.philichkin@gmail.com>
2021-06-04 15:42:56 +0300

    Update myself.js

--- src/myself.js
+++ src/myself.js
@@ -1,26 +1,30 @@
-// const counter = {
-//   value: 0,
-//   increment(value) {
-//     console.log('increment -> this', this);
-//     this.value += value;
-//   },
-//   decrement(value) {
-//     console.log('decrement -> this', this);
-//     this.value -= value;
-//   },
-// };
-
-// const updateCounter = function (value, operation) {
-//   operation(value);
-// };
-
-// updateCounter(10, counter.increment);
-// updateCounter(5, counter.decrement);
-
-const fn = function () {
-  return 5;
+const counter = {
+  value: 0,
+  increment(value) {
+    console.log('increment -> this', this);
+    this.value += value;
+  },
+  decrement(value) {
+    console.log('decrement -> this', this);
+    this.value -= value;
+  },
 };
-// Вызов функции
-console.log(fn());
-// обращение к функции
-console.log(fn);
+
+const updateCounter = function (value, operation) {
+  operation(value);
+};
+
+// updateCounter(10, counter.increment); был без бинда
+updateCounter(10, counter.increment.bind(counter));
+// updateCounter(5, counter.decrement); был без бинда
+updateCounter(5, counter.decrement.bind(counter));
+
+console.log(counter);
+// =======================================================
+// const fn = function () {
+//   return 5;
+// };
+// // Вызов функции
+// console.log(fn());
+// // обращение к функции
+// console.log(fn);
