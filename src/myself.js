91eb6f5bae3801e2bd056b82cac206aa508878c8
Denys Filichkin <denys.philichkin@gmail.com>
2021-06-10 21:18:32 +0300

    Был близок к разгадке

--- src/myself.js
+++ src/myself.js
@@ -86,13 +86,13 @@
 // ===================================================
 
 // ======   Отложенный вызов регистрация событий =====
-const buttonRef = document.querySelector('.js-button');
+// const buttonRef = document.querySelector('.js-button');
 
-const handleBtnClick = function () {
-  console.log('Клик по кнопке' + Date.now());
-};
+// const handleBtnClick = function () {
+//   console.log('Клик по кнопке' + Date.now());
+// };
 
-buttonRef.addEventListener('click', handleBtnClick);
+// buttonRef.addEventListener('click', handleBtnClick);
 
 // мщжно записать коротко инлайн, убрав переменную.
 
@@ -101,3 +101,61 @@ buttonRef.addEventListener('click', handleBtnClick);
 // buttonRef.addEventListener('click', function () {
 //   console.log('Клик по кнопке' + Date.now());
 // });
+
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+class Admin extends User {
+  // Пиши код ниже этой строки
+
+  static AccessLevel = {
+    BASIC: 'basic',
+    SUPERUSER: 'superuser',
+  };
+
+  accessLevel;
+
+  constructor({ email, accessLevel }) {
+    super(email);
+    this.accessLevel = accessLevel;
+  }
+
+  blacklistedEmails = [];
+
+  blacklist(email) {
+    this.blacklistedEmails.push(email);
+  }
+
+  isBlacklisted(email) {
+    if (this.blacklistedEmails.includes(email)) {
+      return true;
+    }
+    return false;
+  }
+
+  // Пиши код выше этой строки
+}
+
+const mango = new Admin({
+  email: 'mango@mail.com',
+  accessLevel: Admin.AccessLevel.SUPERUSER,
+});
+
+console.log(mango.email); // mango@mail.com
+console.log(mango.accessLevel); // superuser
+mango.blacklist('poly@mail.com');
+console.log(mango.blacklistedEmails); // 'poly@mail.com'
+console.log(mango.isBlacklisted('mango@mail.com')); //  false
+console.log(mango.isBlacklisted('poly@mail.com')); // true
