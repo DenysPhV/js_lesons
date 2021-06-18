56c3a14ed264e23d32a3a0bb26461c2f8da4a763
Denys Filichkin <denys.philichkin@gmail.com>
2021-06-18 18:33:13 +0300

    не сам

--- src/myself.js
+++ src/myself.js
@@ -178,15 +178,30 @@
 // console.log(doubledNumbers); // [2, 4, 6, 8, 10]
 
 //  Код более декларативный
-const numbers = [1, 2, 3];
+// const numbers = [1, 2, 3];
 
 // Классический for
-for (let i = 0; i < numbers.length; i += 1) {
-  console.log(numbers[i]);
-}
+// for (let i = 0; i < numbers.length; i += 1) {
+//   console.log(numbers[i]);
+// }
 
 // Функциональный forEach
-numbers.forEach(num => console.log(num));
+// numbers.forEach(num => console.log(num));
 
 // Указываем параметр idx если нужен доступ к счетчику
-numbers.forEach((num, idx) => console.log(`index ${idx}, value ${num}`));
+// numbers.forEach((num, idx) => console.log(`index ${idx}, value ${num}`));
+
+const books = [
+  { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
+  { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
+  { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
+  { title: 'Красна как кровь', author: 'Ли Танит', rating: 8.14 },
+  { title: 'Сны В Ведьмином Доме', author: 'Говард Лавкрафт', rating: 8.67 },
+];
+const MIN_BOOK_RATING = 8;
+// Пиши код ниже этой строки
+
+const names = [...books]
+  .filter(book => book.rating > MIN_BOOK_RATING)
+  .map(book => book.author)
+  .sort((firstAuthor, secondAuthor) => firstAuthor.author.localeCompare(secondAuthor.author));
